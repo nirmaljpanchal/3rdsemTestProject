b0d4abeca773091406e97641bd6b5aa67a225044
Nirmal Panchal <nirmaljpanchal@gmail.com>
2017-11-10 19:18:55 -0500

    new UI design

--- Content/js/main.js
+++ Content/js/main.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
 	
 	// main menu
-	jQuery('#toggle-menu').click(function(){
-        jQuery(this).next().slideToggle();
+	jQuery('#menu-toggle').click(function(e) {
+        e.preventDefault();
+        jQuery('#wrapper').toggleClass('toggled');
+    });
+
+    // affix header
+    var toggleAffix = function(affixElement, scrollElement, wrapper) {
+        var height = affixElement.outerHeight(),
+            top = wrapper.offset().top;
+          
+        if (scrollElement.scrollTop() >= top){
+            wrapper.height(height);
+            affixElement.addClass("affix");
+        } else {
+            affixElement.removeClass("affix");
+            wrapper.height('auto');
+        }  
+    };
+        
+    jQuery('[data-toggle="affix"]').each(function() {
+        var ele = $(this),
+            wrapper = $('<div></div>');
+          
+        ele.before(wrapper);
+        jQuery(window).on('scroll resize', function() {
+            toggleAffix(ele, $(this), wrapper);
+        });
+          
+        // init
+        toggleAffix(ele, $(window), wrapper);
     });
 	
 });
